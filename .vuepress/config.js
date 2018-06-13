9f0f4c51c97fcd01f774c921ce3dcd0569650526
shengxinjing <316783812@qq.com>
2018-06-13 14:02:21 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,6 +4,42 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: `/logo.ico` }]
   ],
-  ga:"UA-120821049-1"
+  ga:"UA-120821049-1",
+  themeConfig: {
+    repo: 'shengxinjing',
+    lastUpdated: 'Last Updated',
+    docsDir: 'docs',
+    nav: [
+      { text: 'javascript', link: '/javascript/' },
+      { text: 'react', link: '/react/' },
+      { text: 'API', link: '/api/' },
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: '指南',
+          collapsable: false,
+          children: [
+            '',
+            'getting-started',
+            'examples-and-boilerplates',
+            'app-structure',
+            'router',
+            'navigate-between-pages',
+            'config',
+            'add-404-page',
+            'html-template',
+            'with-dva',
+            'load-on-demand',
+            'deploy',
+            'env-variables',
+            'faq',
+          ],
+        },
+      ],
+
+    }
+  },
+
 
 }

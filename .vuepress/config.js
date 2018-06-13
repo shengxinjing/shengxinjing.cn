74dbc43e8adb2fbeda62fc99476c8fa9e9fca7fb
shengxinjing <316783812@qq.com>
2018-06-13 13:41:41 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
   title: '蜗牛老湿的个人网站',
-  description: '资深程序员，擅长javascript，python'
+  description: '资深程序员，擅长javascript，python',
+  head: [
+    ['link', { rel: 'icon', href: `/logo.ico` }]
+  ]
 }

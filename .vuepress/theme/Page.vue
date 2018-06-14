ce28ecac574fdd9ff603360edfd1c51caa9e003a
shengxinjing <316783812@qq.com>
2018-06-15 02:05:47 +0800

    deploy

--- .vuepress/theme/Page.vue
+++ .vuepress/theme/Page.vue
@@ -25,8 +25,9 @@
         </span>
       </p>
     </div>
-    
     <slot name="bottom"/>
+    <div id="SOHUCS" sid="id" ></div> 
+    
   </div>
 </template>
 
@@ -35,6 +36,14 @@ import { resolvePage, normalize, outboundRE, endingSlashRE } from './util'
 
 export default {
   props: ['sidebarItems'],
+  mounted(){
+    (function(){ 
+var appid = 'cytoUBPhR'; 
+var conf = 'prod_e9c3098361c8007ab622a289cead0727'; 
+var width = window.innerWidth || document.documentElement.clientWidth; 
+if (width < 960) { 
+window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("https://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); } })(); 
+  },
   computed: {
     lastUpdated () {
       if (this.$page.lastUpdated) {

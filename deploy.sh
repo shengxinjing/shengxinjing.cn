ef7c6f1f8787578793bc91ea9199424b56840393
shengxinjing <316783812@qq.com>
2019-03-03 17:40:40 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 # 生成静态文
-npm run build
+# npm run build
 
 # 进入生成的文件夹
 # cd docs/.vuepress/dist
@@ -15,5 +15,5 @@ git add -A
 git commit -m 'deploy'
 
 git push origin master
-ssh root@woniu "cd /root/shengxinjing.cn;git pull"
+ssh root@woniu "cd /root/shengxinjing.cn;git pull;npm run build"
 exit

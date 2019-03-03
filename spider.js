aa562d8eb2aed47762aae04e78ca3dd8259f276a
shengxinjing <316783812@qq.com>
2019-03-03 15:57:46 +0800

    ix

--- spider.js
+++ spider.js
@@ -1,6 +1,6 @@
 let request = require('request')
 let fs = require('fs')
-const url = 'https://api.github.com/repos/shengxinjing/my_blog/issues?access_token=5de13e577661d05daaa9995a4c60d46d4f5c81c7&labels=gitment'
+const url = 'https://api.github.com/repos/shengxinjing/my_blog/issues?access_token=f723c5d1a424d594544e83be081ca519a9d258c2&labels=gitment'
 
 // 发送Get请求
 // 第一个参数:请求的完整URL,包括参数
@@ -12,7 +12,7 @@ request({
     }
 
 },function (error, response, data) {
-   
+    console.log(data)
     fs.writeFileSync('.vuepress/theme/article.json',data)
 });
 
